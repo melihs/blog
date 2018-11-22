e9e642e843407ab01f27b56e963ff59b3950ed6f
melih sahin <melihsahin24@gmail.com>
2018-11-22 21:54:38 +0300

    created were model files

--- app/User.php
+++ app/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    protected $guarded = [ ];
+
     /**
      * The attributes that should be hidden for arrays.
      *

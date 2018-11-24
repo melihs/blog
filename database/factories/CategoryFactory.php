d7cb7b5f2b13063180dc7324a6473283082467de
melih sahin <melihsahin24@gmail.com>
2018-11-24 17:20:25 +0300

    CategoryFactory file was added required namespaces

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,8 +1,9 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Category;
 
-$factory->define(App\Category::class, function (Faker $faker) {
+$factory->define(Category::class, function (Faker $faker) {
     $title=$faker->sentence;
     return [
             'title' =>$title

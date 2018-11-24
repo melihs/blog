f91a96a2fc772f851d63a26f681b96c83190f8c1
melih sahin <melihsahin24@gmail.com>
2018-11-24 16:56:42 +0300

    CategoryFactory and PostFactory files were completed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,8 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
+    $title=$faker->sentence;
     return [
-
+            'title' =>$title
     ];
 });
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    $title=$faker->sentence;
     return [
-        //
+        'title' => $title,
+        'content' =>$faker->text,
+        'category_id' => function()
+        {
+            return Category::all()->random();
+        },
+        'user_id' => function ()
+        {
+            return User::all()->random();
+        }
     ];
 });

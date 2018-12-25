df3455bae747edbde99c5154be1111a8e244fe7c
melih sahin <melihsahin24@gmail.com>
2018-12-25 12:54:54 +0300

    removed relations detail and role

--- app/User.php
+++ app/User.php
@@ -11,14 +11,13 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable
-     *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = [ 'name', 'email', 'password'];
 
+    /**
+     * @var array
+     */
     protected $guarded = [ ];
 
     /**
@@ -26,25 +25,24 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = [ 'password', 'remember_token'];
 
-    //kullanıcıya ait yazıları bulmak için
+    /**
+     * @return HasMany relations
+     */
     public function posts()
     {
-//       return $this->hasMany('App\Post','user_id','id');
          return $this->hasMany('App\Post');
     }
 
-    public function details(  )
-    {
-        return $this->hasOne('App\Detail','user_id','id ');
-    }
-
-    public function roles(  )
-    {
-        return $this->belongsToMany('App\Role');
-    }
+//    public function details(  )
+//    {
+//        return $this->hasOne('App\Detail','user_id','id ');
+//    }
+//
+//    public function roles(  )
+//    {
+//        return $this->belongsToMany('App\Role');
+//    }
 
 }
